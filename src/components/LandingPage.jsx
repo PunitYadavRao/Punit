7f8efbe83eda3a97a8e148218d3e56a7bfeb4159
Punit Personal <punitp@Punits-MacBook-Pro.local>
2025-03-22 13:12:51 +0530

    Img Link imbb update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,7 +4,7 @@ const LandingPage = ({theme}) => {
   return (
     <>
         <div className={`flex flex-col justify-center items-center ${theme} text-5xl font-bold pt-20`}>
-            <img className='h-100 w-auto rounded-3xl' src="./src/assets/YoPunit.jpg" alt="Yo punit thara bhai putin" />
+            <img className='h-80 w-auto rounded-3xl' src="https://i.postimg.cc/vcCcsnyN/YoPunit.jpg" alt="Yo punit thara bhai putin" />
             <p>Yo Punit, thara bhai Punit,</p>
             <p>itni coding kr rya hai, thane sochi na hogi!!!</p>
         </div>
